cfc8ae08c7ca4fa0036f378d7bd40d5c6279e21a
Hiroki Fujii <hfujii@hisystron.com>
2021-12-15 11:10:19 +0900

    予約終了時刻指定に対応

--- src/contents/main/createReserve.jsx
+++ src/contents/main/createReserve.jsx
@@ -3,11 +3,16 @@ import { Button } from "react-bootstrap";
 
 import apiUtil from "../../utils/apiUtil";
 
-const stateIsValid = (state) => {
-    if ((state.hour === "none") || (state.minute === "none")) {
+const stateIsValid = (props, state) => {
+    if ((state.startHour === "none") || (state.startMinute === "none") || (state.endHour === "none") || (state.endMinute === "none")) {
         return false;
     }
     if (state.name.length <= 5) {
+        return false;
+    }
+    let startAt = new Date(props.state.date + " " + state.startHour + ":" + state.startMinute);
+    let endAt = new Date(props.state.date + " " + state.endHour + ":" + state.endMinute);
+    if (startAt.getTime() >= endAt.getTime()) {
         return false;
     }
     return true;
@@ -30,16 +35,28 @@ const CreateReserve = (props) => {
     const now = new Date();
 
     // 時刻ココンボの作成
-    let hours = [(<option id="hour_none" value="none">時間を選択</option>)];
-    let minutes = [(<option id="minute_none" value="none">分を選択</option>)];
+    let hours = [(<option key="hour_none" value="none">時間を選択</option>)];
+    let minutes = [(<option key="minute_none" value="none">分を選択</option>)];
     for (let i = now.getHours(); i <= 23; i++){
-        hours.push(<option id={"hour_" + i} value={i}>{("00" + i).slice(-2)}</option>);
+        hours.push(<option key={"starthour_" + i} value={i}>{("00" + i).slice(-2)}</option>);
     }
     for (let i1 = 0; i1 < 60; i1 += 5) {
-        if ((now.getHours() === parseInt(formState.hour)) && (now.getMinutes() >= i1)) {
+        if ((now.getHours() === parseInt(formState.startHour)) && (now.getMinutes() >= i1)) {
             continue;
         }
-        minutes.push(<option id={"minutes_" + i1} value={i1}>{("00" + i1).slice(-2)}</option>);
+        minutes.push(<option key={"startminutes_" + i1} value={i1}>{("00" + i1).slice(-2)}</option>);
+    }
+
+    let endHours = [(<option key="hour_none" value="none">時間を選択</option>)];
+    let endMinutes = [(<option key="minute_none" value="none">分を選択</option>)];
+    for (let i = now.getHours(); i <= 23; i++){
+        endHours.push(<option key={"endhour_" + i} value={i}>{("00" + i).slice(-2)}</option>);
+    }
+    for (let i1 = 0; i1 < 60; i1 += 5) {
+        if ((now.getHours() === parseInt(formState.endHour)) && (now.getMinutes() >= i1)) {
+            continue;
+        }
+        endMinutes.push(<option key={"endminutes_" + i1} value={i1}>{("00" + i1).slice(-2)}</option>);
     }
 
     const handleChange = (e) => {
@@ -51,8 +68,8 @@ const CreateReserve = (props) => {
         apiUtil.createReservation(setPageState, {
             name: formState.name,
             roomId: props.state.roomId,
-            startAt: new Date(props.state.date + " " + formState.hour + ":" + formState.minute),
-            endAt: new Date(props.state.date + " " + (parseInt(formState.hour) + 1) + ":" + formState.minute),
+            startAt: new Date(props.state.date + " " + formState.startHour + ":" + formState.startMinute),
+            endAt: new Date(props.state.date + " " + formState.endHour + ":" + formState.endMinute),
             type: 0,
             userInfo: {
                 id: 1
@@ -62,7 +79,7 @@ const CreateReserve = (props) => {
 
 
     // 予約作成ボタンの制御
-    let createButton = stateIsValid(formState);
+    let createButton = stateIsValid(props, formState);
     let createButtonValue = "予約作成";
     if (pageState === "loading") {
         createButton = false;
@@ -78,8 +95,10 @@ const CreateReserve = (props) => {
     }
     
     return (<>
-        <select name="hour" value={formState.hour} onChange={handleChange}>{hours}</select>
-        <select name="minute" value={formState.minute} onChange={handleChange}>{minutes}</select>
+        <select name="startHour" value={formState.startHour} onChange={handleChange}>{hours}</select>
+        <select name="startMinute" value={formState.startMinute} onChange={handleChange}>{minutes}</select>
+        <select name="endHour" value={formState.endHour} onChange={handleChange}>{endHours}</select>
+        <select name="endMinute" value={formState.endMinute} onChange={handleChange}>{endMinutes}</select>
         <input id="reserver" name="name" value={formState.name} onChange={handleChange} />
         {createButton && <Button onClick={()=>{handleSubmit()}} variant="success">{createButtonValue}</Button>}
     </>);
